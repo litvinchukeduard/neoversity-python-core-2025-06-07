c6b32687eb1d3939c5f6fecb16fe1c3028d6c3f8
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-06-07 15:32:22 +0300

    Get budget

---
+++ practice_one.py
@@ -0,0 +1,34 @@
+'''
+Потрібно написати код, який допоможе керувати бюджетом
+
+$100
+
+Вводимо бюджет на тиждень
+
+Вводимо витрати на денб
+'''
+
+# Hello world
+
+# First step: Enter budget
+# weekly_budget = input("Enter budget for the week: ")
+# print(weekly_budget)
+
+#hello 
+# First step: Enter budget
+
+# Ctrl /
+# COMMAND /
+# weekly_budget = input("Enter budget for the week :") # not int, but str "100.16" "100"
+# weekly_budget = int(weekly_budget)
+
+try:
+    weekly_budget = int(input("Enter budget for the week :")) # not int, but str "100.16" "100"
+except ValueError:
+    print("Weekly budget value is not correct. We are setting budget to 100...")
+    weekly_budget = 100
+
+print(weekly_budget + 100)
+
+# '300a'
+
