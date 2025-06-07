ec0635e3137ac59f92877058ac9878b4b0c012f9
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-06-07 16:26:41 +0300

    Max expenses

--- practice_two.py
+++ practice_two.py
@@ -34,4 +34,30 @@ expenses = []
 for day in DAYS_OF_THE_WEEK:
     expenses.append(get_number_from_user_with_prompt("Enter " + day + " expenses: ", 100))
 
+# sum([1, 2, 3, 4])
 
+# sum = 0
+# list = []
+
+sum_of_expenses = 0
+for expense in expenses:
+    sum_of_expenses += expense
+
+expenses_difference = weekly_budget - sum_of_expenses
+print("Total expenses: " + str(sum_of_expenses))
+print("Difference: " + str(expenses_difference))
+
+# max_expense = max(expenses)
+# print(max_expense)
+
+# enumerate
+
+max_index = 0
+max_expense = 0
+for i in range(len(expenses)):
+    expense = expenses[i]
+    if expense > max_expense:
+        max_index = i
+        max_expense = expense
+
+print("On " + DAYS_OF_THE_WEEK[max_index] + " you spent " + str(max_expense))
