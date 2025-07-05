f441a89ef3812b7ad23b680219142d7ee983669b
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-07-05 16:05:05 +0300

    Call method example

--- three/playlist.py
+++ three/playlist.py
@@ -26,14 +26,24 @@ class Playlist:
     def __iadd__(self, song: Song):
         self.add_song(song)
         return self
+    
+    def __call__(self):
+        # sum is way better
+        # total_seconds = 0
+        # for song in self.songs:
+        #     total_seconds += song.duration_in_seconds
+        # return total_seconds
+        # generator = (song.duration_in_seconds for song in self.songs)
+        # print(generator)
+        return sum(song.duration_in_seconds for song in self.songs)
 
     def __str__(self) -> str:
         return f'Playlist(name="{self.name}", songs={self.songs})'
 
 
 if __name__ == '__main__':
-    song_one = Song("Author One", "Song One", 0, Genre.POP)
-    song_two = Song("Author Two", "Song Two", 0, Genre.ROCK)
+    song_one = Song("Author One", "Song One", 100, Genre.POP)
+    song_two = Song("Author Two", "Song Two", 300, Genre.ROCK)
 
     playlist_one = Playlist("Playlist one")
     # playlist_one.add_song(song_one)
@@ -42,6 +52,8 @@ if __name__ == '__main__':
     print(playlist_one)
     playlist_one += song_two
 
-    print(playlist_one)
+    print(playlist_one())
+
+    # playlist_one("John", "Doe")
     # my_list = [1, 2, 3]
     # print(my_list[::-1])
